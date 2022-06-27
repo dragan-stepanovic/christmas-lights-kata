6f952e40a27e9dbd0c6e2d14d2dc2dda8bb8b8bf
Dragan Stepanović <dragan.stepanovic@gmail.com>
2022-06-27 21:31:36 +0200

    xv

--- Kata.Tests/HelloWorldTests.cs
+++ Kata.Tests/HelloWorldTests.cs
@@ -9,7 +9,7 @@ namespace Kata.Tests
         {
             var lights = new Lights(0);
             Assert.Equal(1, lights.TurnOn());
-            Assert.Equal(1, new Lights(1).TurnOn());
+            Assert.Equal(1, lights.TurnOn());
 
             Assert.Equal(0, new Lights(1).TurnOff());
             Assert.Equal(0, new Lights(0).TurnOff());
