e4f0390d0ea1aa2b68ee0861b57c76e85eea5a2c
Dragan Stepanović <dragan.stepanovic@gmail.com>
2022-06-27 21:25:24 +0200

    passing

--- Kata.Tests/HelloWorldTests.cs
+++ Kata.Tests/HelloWorldTests.cs
@@ -10,7 +10,7 @@ namespace Kata.Tests
             Assert.Equal(1, Lights.TurnOn());
             Assert.Equal(0, Lights.TurnOff());
             Assert.Equal(1, Lights.Toggle());
-            Assert.Equal(0, Lights.Toggle());
+            Assert.Equal(0, Lights.Toggle(1));
         }
     }
 
@@ -26,8 +26,11 @@ namespace Kata.Tests
             return 0;
         }
 
-        public static int Toggle()
+        public static int Toggle(int was = 0)
         {
+            if (was == 1)
+                return 0;
+
             return 1;
         }
     }
