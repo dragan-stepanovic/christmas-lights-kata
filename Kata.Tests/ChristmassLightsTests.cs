be4ee1ed2d8e52c6be7f2c36e8169dca5fa0cfa9
Dragan Stepanović <dragan.stepanovic@gmail.com>
2022-06-28 21:06:13 +0200

    refactor tests

--- Kata.Tests/ChristmassLightsTests.cs
+++ Kata.Tests/ChristmassLightsTests.cs
@@ -20,11 +20,11 @@ namespace Kata.Tests
             AssertThatAfter(lights.Toggle(), WeHave(1));
             AssertThatAfter(lights.Toggle(), WeHave(0));
 
-            lights.On().Should().BeEquivalentTo(new[] {1});
-            lights.Toggle().Should().BeEquivalentTo(new[] {0});
-            lights.On().Should().BeEquivalentTo(new[] {1});
-            lights.Off().Should().BeEquivalentTo(new[] {0});
-            lights.Toggle().Should().BeEquivalentTo(new[] {1});
+            AssertThatAfter(lights.On(), WeHave(1));
+            AssertThatAfter(lights.Toggle(), WeHave(0));
+            AssertThatAfter(lights.On(), WeHave(1));
+            AssertThatAfter(lights.Off(), WeHave(0));
+            AssertThatAfter(lights.Toggle(), WeHave(1));
         }
 
         [Fact]
