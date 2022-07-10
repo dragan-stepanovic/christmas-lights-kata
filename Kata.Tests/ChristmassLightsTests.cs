a4f6761bcd26e1d76c37d2a999efb1a613201cf7
Dragan Stepanović <dragan.stepanovic@gmail.com>
2022-07-10 21:52:20 +0200

    remove tests

--- Kata.Tests/ChristmassLightsTests.cs
+++ Kata.Tests/ChristmassLightsTests.cs
@@ -7,39 +7,6 @@ namespace Kata.Tests
     public class HelloWorldTests
     {
         [Fact]
-        public void ManipulatesNLightsInTheSameRow()
-        {
-            Lights(0, 0, 0).AllOn().Should().Be(Lights(1, 1, 1));
-            Lights(0, 0, 0).AllOn().Should().Be(Lights(1, 1, 1));
-            Lights(1, 1, 1).AllOff().Should().Be(Lights(0, 0, 0));
-            Lights(0, 0, 0).ToggleAll().Should().Be(Lights(1, 1, 1));
-            Lights(1, 1, 1).ToggleAll().Should().Be(Lights(0, 0, 0));
-            Lights(1, 0, 1).ToggleAll().Should().Be(Lights(0, 1, 0));
-            Lights(0, 0, 0).AllOn().Should().Be(Lights(1, 1, 1));
-            Lights(1, 1, 1).AllOff().Should().Be(Lights(0, 0, 0));
-            Lights(0, 0, 0, 0, 0).AllOn().Should().Be(Lights(1, 1, 1, 1, 1));
-        }
-
-        [Fact]
-        public void ManipulateIndividualLightsInTheSameRow()
-        {
-            Lights(0, 0).On(new[] {1, 1}).Should().Be(Lights(0, 1));
-            Lights(0, 0).On(new[] {0, 0}).Should().Be(Lights(1, 0));
-            Lights(0, 0).On(new[] {0, 1}).Should().Be(Lights(1, 1));
-            Lights(0, 0, 0).On(new[] {0, 1}).Should().Be(Lights(1, 1, 0));
-            Lights(0, 0, 0, 0).On(new[] {1, 2}).Should().Be(Lights(0, 1, 1, 0));
-            Lights(0, 1, 0, 1).On(new[] {1, 2}).Should().Be(Lights(0, 1, 1, 1));
-            Lights(0, 1, 0, 1).On(new[] {0, 3}).Should().Be(Lights(1, 1, 1, 1));
-
-            Lights(1, 1).Off(new[] {1, 1}).Should().Be(Lights(1, 0));
-            Lights(1, 1).Off(new[] {0, 1}).Should().Be(Lights(0, 0));
-            Lights(1, 1, 1).Off(new[] {2, 2}).Should().Be(Lights(1, 1, 0));
-            Lights(1, 1, 1).Off(new[] {0, 2}).Should().Be(Lights(0, 0, 0));
-            Lights(0, 1, 0, 1).Off(new[] {0, 3}).Should().Be(Lights(0, 0, 0, 0));
-            Lights(0, 1, 0, 1).Off(new[] {1, 2}).Should().Be(Lights(0, 0, 0, 1));
-        }
-
-        [Fact]
         public void ManipulateAllLightsInMultipleRows()
         {
             new Lights(new[,] {{0}}).On(new[] {0, 0}).Should().Be(new Lights(new[,] {{1}}));
