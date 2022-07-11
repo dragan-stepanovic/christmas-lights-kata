819f4fe40da74013fd310f5119543f2c74dda6b3
Dragan Stepanović <dragan.stepanovic@gmail.com>
2022-07-11 14:15:53 +0200

    remove dup

--- Kata.Tests/ChristmassLightsTests.cs
+++ Kata.Tests/ChristmassLightsTests.cs
@@ -1,4 +1,3 @@
-using Castle.Core;
 using FluentAssertions;
 using Xunit;
 
@@ -9,17 +8,17 @@ namespace Kata.Tests
         [Fact]
         public void TurningOn()
         {
-            new Lights(new[,] {{0}}).TurnOnBetween(Tests.Coordinate.ZeroZero(), Coordinate(0, 0)).Should()
+            new Lights(new[,] {{0}}).TurnOnBetween(Coordinate.ZeroZero(), Coordinate.At(0, 0)).Should()
                 .Be(new Lights(new[,] {{1}}));
-            new Lights(new[,] {{0, 0}}).TurnOnBetween(Tests.Coordinate.ZeroZero(), Coordinate(0, 1)).Should()
+            new Lights(new[,] {{0, 0}}).TurnOnBetween(Coordinate.ZeroZero(), Coordinate.At(0, 1)).Should()
                 .Be(new Lights(new[,] {{1, 1}}));
-            new Lights(new[,] {{0, 0, 0}}).TurnOnBetween(Tests.Coordinate.ZeroZero(), Coordinate(0, 2)).Should()
+            new Lights(new[,] {{0, 0, 0}}).TurnOnBetween(Coordinate.ZeroZero(), Coordinate.At(0, 2)).Should()
                 .Be(new Lights(new[,] {{1, 1, 1}}));
             new Lights(new[,]
                 {
                     {0, 0, 0},
                     {0, 1, 0}
-                }).TurnOnBetween(Tests.Coordinate.ZeroZero(), Coordinate(1, 1))
+                }).TurnOnBetween(Coordinate.ZeroZero(), Coordinate.At(1, 1))
                 .Should().Be(new Lights(new[,]
                 {
                     {1, 1, 0},
@@ -30,7 +29,7 @@ namespace Kata.Tests
                 {
                     {0, 0, 0},
                     {0, 1, 0}
-                }).TurnOnBetween(Tests.Coordinate.ZeroZero(), Coordinate(1, 2))
+                }).TurnOnBetween(Coordinate.ZeroZero(), Coordinate.At(1, 2))
                 .Should().Be(new Lights(new[,]
                 {
                     {1, 1, 1},
@@ -42,7 +41,7 @@ namespace Kata.Tests
                     {0, 0, 0},
                     {0, 1, 0},
                     {1, 1, 0}
-                }).TurnOnBetween(Tests.Coordinate.ZeroZero(), Coordinate(2, 1))
+                }).TurnOnBetween(Coordinate.ZeroZero(), Coordinate.At(2, 1))
                 .Should().Be(new Lights(new[,]
                 {
                     {1, 1, 0},
@@ -55,7 +54,7 @@ namespace Kata.Tests
                     {0, 0, 0, 0},
                     {0, 1, 0, 1},
                     {1, 1, 0, 0}
-                }).TurnOnBetween(Tests.Coordinate.ZeroZero(), Coordinate(2, 3))
+                }).TurnOnBetween(Coordinate.ZeroZero(), Coordinate.At(2, 3))
                 .Should().Be(new Lights(new[,]
                 {
                     {1, 1, 1, 1},
@@ -72,7 +71,7 @@ namespace Kata.Tests
                     {0, 0, 0, 0},
                     {0, 1, 0, 1},
                     {1, 1, 0, 0}
-                }).TurnOffBetween(Coordinate(1, 1), Coordinate(2, 3))
+                }).TurnOffBetween(Coordinate.At(1, 1), Coordinate.At(2, 3))
                 .Should().Be(new Lights(new[,]
                 {
                     {0, 0, 0, 0},
@@ -85,7 +84,7 @@ namespace Kata.Tests
                     {0, 0, 1, 0},
                     {1, 1, 0, 1},
                     {1, 1, 0, 1}
-                }).TurnOffBetween(Tests.Coordinate.ZeroZero(), Coordinate(2, 3))
+                }).TurnOffBetween(Coordinate.ZeroZero(), Coordinate.At(2, 3))
                 .Should().Be(new Lights(new[,]
                 {
                     {0, 0, 0, 0},
@@ -102,7 +101,7 @@ namespace Kata.Tests
                     {0, 0, 0, 0},
                     {0, 1, 0, 1},
                     {1, 1, 0, 0}
-                }).ToggleBetween(Coordinate(1, 1), Coordinate(2, 3))
+                }).ToggleBetween(Coordinate.At(1, 1), Coordinate.At(2, 3))
                 .Should().Be(new Lights(new[,]
                 {
                     {0, 0, 0, 0},
@@ -116,11 +115,6 @@ namespace Kata.Tests
         {
             new Lights(new[,] {{1, 0, 1}}).ToString().Should().Be("1, 0, 1");
             new Lights(new[,] {{1, 0, 1}, {1, 0, 0}}).ToString().Should().Be("1, 0, 1\n1, 0, 0");
-        }
-
-        private static Pair<int, int> Coordinate(int first, int second)
-        {
-            return new Pair<int, int>(first, second);
         }
     }
 }
